9f4e9ee1a1def9ddede9e6269b9022319644dc67
Dan Beil <dan.beil@blueearth.net>
2013-10-24 17:11:31 -0500

    changed db to be wpboston-2013.sql, updated wp-config

--- wcboston-wp/wp-config.php
+++ wcboston-wp/wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'test');
+define('DB_NAME', 'wpboston-2013');
 
 
 /** MySQL database username */
